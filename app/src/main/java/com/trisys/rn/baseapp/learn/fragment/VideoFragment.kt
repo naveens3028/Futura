4a254d5480d679a5e492c6304247593c9d4e71d7
OFFICE <naveen.s@changepond.com>
2021-06-12 16:26:26 +0530

    - Exoplayer added

--- app/src/main/java/com/trisys/rn/baseapp/learn/fragment/VideoFragment.kt
+++ app/src/main/java/com/trisys/rn/baseapp/learn/fragment/VideoFragment.kt
@@ -1,16 +1,16 @@
 package com.trisys.rn.baseapp.learn.fragment
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.trisys.rn.baseapp.R
-import kotlinx.android.synthetic.main.fragment_study_material.*
-
+import com.vpnews24.utils.ImageLoader
+import kotlinx.android.synthetic.main.fragment_video.*
 
 class VideoFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +20,12 @@ class VideoFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_video, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val path = "android.resource://" + requireContext().getPackageName() + "/" + R.raw.son
+        andExoPlayerView.setSource(Uri.parse(path).toString())
+
+    }
 
 }
