4ed7669d7cbc8f6cf3288923a58370ff4688eee0
naveens3028 <naveens3028@gmail.com>
2021-10-24 13:58:39 +0530

    completed live changes

--- app/src/main/java/com/trisys/rn/baseapp/adapter/CompletedLiveAdapter.kt
+++ app/src/main/java/com/trisys/rn/baseapp/adapter/CompletedLiveAdapter.kt
@@ -6,29 +6,82 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.trisys.rn.baseapp.R
+import com.trisys.rn.baseapp.fragment.CompletedLiveAdapterListener
 import com.trisys.rn.baseapp.model.onBoarding.CompletedSession
-import kotlinx.android.synthetic.main.row_completed_live.view.*
+import kotlinx.android.synthetic.main.row_complete_live.view.*
 
 
 class CompletedLiveAdapter(
     val context: Context,
-    private val completedLive : ArrayList<CompletedSession>,
+    private val completedLive : List<CompletedSession>,
+    private val listener: CompletedLiveAdapterListener
 ) : RecyclerView.Adapter<CompletedLiveAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v =
-            LayoutInflater.from(parent.context).inflate(R.layout.row_completed_live, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.row_complete_live, parent, false)
         return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val completedLive = completedLive[position]
-        holder.itemView.subject.text = completedLive.courseName
-        holder.itemView.backgroundColor.setBackgroundColor(context.getColor(R.color.mikado_yellow))
-        holder.itemView.lesson.text = completedLive.description
+        holder.itemView.subjectTxt.text = completedLive.courseName
+
+        holder.itemView.cardComplete.setOnClickListener {
+            listener.onClicked(completedLive)
+        }
+
+        when(completedLive.courseName){
+            "CHEMISTRY"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_color_chem)
+            }
+            "PHYSICS"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_color_phy)
+            }
+            "BIOLOGY"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_color_bio)
+            }
+            "TAMIL"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_tamil)
+            }
+            "URDU"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_urdu)
+            }
+            "SOCIAL SCIENCE"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_social)
+            }
+            "MATHEMATICS"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_color_math)
+            }
+            "ACCOUNTANCY"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_accoutancy)
+            }
+            "ENGLISH"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_english)
+            }
+            "HINDI"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_hindi)
+            }
+            "SCIENCE"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_science)
+            }
+            "KANNADA"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_kannada)
+            }
+            "HISTORY"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_history)
+            }
+            "ECONOMICS"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_economics)
+            }
+            "COMPUTER SCIENCE"->{
+                holder.itemView.subjectimg.setImageResource(R.drawable.ic_comp_sci)
+            }
+        }
+
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/trisys/rn/baseapp/fragment/CompletedLiveFragment.kt
+++ app/src/main/java/com/trisys/rn/baseapp/fragment/CompletedLiveFragment.kt
@@ -6,16 +6,17 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import com.trisys.rn.baseapp.R
 import com.trisys.rn.baseapp.adapter.CompletedLiveAdapter
-import com.trisys.rn.baseapp.model.CompletedLiveItem
 import com.trisys.rn.baseapp.model.onBoarding.CompletedSession
 import com.trisys.rn.baseapp.model.onBoarding.LoginData
-import com.trisys.rn.baseapp.network.*
+import com.trisys.rn.baseapp.network.ApiInterface
+import com.trisys.rn.baseapp.network.ApiUtils
+import com.trisys.rn.baseapp.network.RetroFitCall
 import com.trisys.rn.baseapp.utils.Define
 import com.trisys.rn.baseapp.utils.MyPreferences
 import kotlinx.android.synthetic.main.fragment_upcoming_live.*
@@ -24,23 +25,19 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.json.JSONArray
 import org.json.JSONObject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class CompletedLiveFragment : Fragment(), OnNetworkResponse {
+class CompletedLiveFragment : Fragment(), CompletedListener, CompletedLiveAdapterListener {
 
-    lateinit var networkHelper: NetworkHelper
     lateinit var myPreferences: MyPreferences
     private var loginData = LoginData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         myPreferences = MyPreferences(requireContext())
-        networkHelper = NetworkHelper(requireContext())
     }
 
     override fun onCreateView(
@@ -56,7 +53,7 @@ class CompletedLiveFragment : Fragment(), OnNetworkResponse {
 
         loginData =
             Gson().fromJson(myPreferences.getString(Define.LOGIN_DATA), LoginData::class.java)
-        getApiCall(requireContext())
+        getApiCall(requireContext(), this)
 
     }
 
@@ -81,7 +78,7 @@ class CompletedLiveFragment : Fragment(), OnNetworkResponse {
     }
 
 
-    fun getApiCall(context: Context) {
+    private fun getApiCall(context: Context, listener: CompletedListener) {
         val myBatchList = JSONArray()
         loginData.userDetail?.batchList?.forEach {
             myBatchList.put(it.id!!)
@@ -100,68 +97,28 @@ class CompletedLiveFragment : Fragment(), OnNetworkResponse {
             RetroFitCall.retroFitCall()
             val service = RetroFitCall.retrofit.create(ApiInterface::class.java)
             val response = service.getData(jsonObject, ApiUtils.getHeader(context))
-            if (response.isSuccessful){
+            if (response.isSuccessful) {
                 if (response.code() == 200) {
-                    var auditList : List<CompletedSession> = response.body()!!
-                    Log.e("retoCall", response.body().toString())
+                    var auditList: List<CompletedSession> = response.body()!!
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        listener.onSuccess(auditList)
+                    }
+                } else {
+                    showErrorMsg("No Completed Live Sessions")
                 }
-            }else{
-                Log.e("retoCall1", response.isSuccessful.toString())
-
-            }
-        }
-    }
-
-
-    private fun requestSessions() {
-
-        val myBatchList = JSONArray()
-        loginData.userDetail?.batchList?.forEach {
-            myBatchList.put(it.id!!)
-        }
-        val myBranchIDs = JSONArray()
-        loginData.userDetail?.branchList?.forEach {
-            myBranchIDs.put(it.id!!)
-        }
-
-
-        val jsonObject = JSONObject()
-        jsonObject.put("branchIds", myBranchIDs)
-        jsonObject.put("coachingCentreId", loginData.userDetail?.coachingCenterId.toString())
-        jsonObject.put("batchIds", myBatchList)
-
-
-        stateful.showProgress()
-        stateful.setProgressText("")
-        networkHelper.postCallResponseArray(
-            URLHelper.getCompletedSessionsSubject,
-            jsonObject,
-            "completedSessions",
-            ApiUtils.getHeader(requireContext()),
-            this
-        )
-    }
-
-    override fun onNetworkResponse(responseCode: Int, response: String, tag: String) {
-        try {
-            stateful.showContent()
-            if (responseCode == networkHelper.responseSuccess && tag == "completedSessions") {
-                Log.e(
-                    "soppers",
-                    "responseCode: " + responseCode.toString() + "response: " + response + " tag: " + tag
-                )
-                  val arrayTutorialType = object : TypeToken<ArrayList<CompletedSession>>() {}.type
-                  val liveItemResponse: ArrayList<CompletedSession> = Gson().fromJson(response, arrayTutorialType)
-                  liveItemResponse.let {
-                      val completedLiveAdapter = CompletedLiveAdapter(requireContext(), liveItemResponse)
-                      recycler.adapter = completedLiveAdapter
-                  }
             } else {
-                showErrorMsg("No completed sessions available")
+                showErrorMsg("No Completed Live Sessions")
+                Log.e("retoCall1", response.isSuccessful.toString())
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
         }
+    }
+
+    private fun setAdapter(completedLive: List<CompletedSession>) {
+        val completedLiveAdapter = CompletedLiveAdapter(requireContext(), completedLive, this)
+        val layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recycler.layoutManager = layoutManager
+        recycler.adapter = completedLiveAdapter
     }
 
     fun showErrorMsg(errorMsg: String) {
@@ -169,7 +126,27 @@ class CompletedLiveFragment : Fragment(), OnNetworkResponse {
         stateful.setOfflineText(errorMsg)
         stateful.setOfflineImageResource(R.drawable.ic_no_data)
         stateful.setOfflineRetryOnClickListener {
-            getApiCall(requireContext())
+            getApiCall(requireContext(), this)
         }
     }
+
+    override fun onSuccess(auditList: List<CompletedSession>) {
+        setAdapter(auditList)
+    }
+
+    override fun onFailure() {
+    }
+
+    override fun onClicked(completedSession: CompletedSession) {
+
+    }
+}
+
+interface CompletedListener {
+    fun onSuccess(auditList: List<CompletedSession>)
+    fun onFailure()
+}
+
+interface CompletedLiveAdapterListener {
+    fun onClicked(completedSession : CompletedSession)
 }
